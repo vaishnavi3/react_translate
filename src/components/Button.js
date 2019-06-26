dd8f2997612ea738aa6415dc44f7e0a9b1421ead
Vaishnavi <vaishnavi.bhaarde@blimp.co.in>
2019-06-26 12:38:30 +0530

    context with redux too

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,15 +12,15 @@ class Button extends React.Component {
   // declare contextType
 
 
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Voorleggen';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
   }
 
   renderBtn(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
